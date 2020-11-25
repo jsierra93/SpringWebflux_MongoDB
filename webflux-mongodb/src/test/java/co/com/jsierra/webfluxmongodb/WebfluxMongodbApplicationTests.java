45b2a55cd265652611bfcd804fb8ff7ffae7e8d2
jsierra93 <jsierra93@hotmail.com>
2020-11-25 10:04:21 -0500

    Se inicializa proyecto

---
+++ webflux-mongodb/src/test/java/co/com/jsierra/webfluxmongodb/WebfluxMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.jsierra.webfluxmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
